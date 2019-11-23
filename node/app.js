6f89af8f0518154b1b3ccc7cc773e12dc53f0ed3
Klvst3r <klvst3r@yahoo.com>
2019-11-22 20:04:34 -0600

    Create DB, and Insert onject by JSON

--- node/app.js
+++ node/app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 
+var mongoose = require("mongoose");
+
 var app = express();
+
+mongoose.connect("mongodb://localhost/primera_pagina");
+
+
+//Schema de productos
+var productSchema = {
+	title:String,
+	description: String,
+	imageURL: String,
+	pricing: Number
+};
+
+//Modelo
+var Product = mongoose.model("Product", productSchema);
 
 app.set("view engine", "jade");
 
@@ -8,8 +24,20 @@ app.use(express.static("public"))
 
 app.get("/", function(req,res){
   
+	var data = {
+		title: "Mi primer super producto",
+		description: "Una mega super hiper compra",
+		imageURL: "data.png",
+		pricing: 10
+	}
+
+	var product = new Product(data);
+
+	product.save(function(err){
+		console.log(product);
+	});
+
 	res.render('index');
 });
-
 
 app.listen(8081);
